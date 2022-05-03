70ed3a4dd989f0b8f0467229a88906c2d2f25a1c
JimmyCamus <jeremy.camus.varela@gmail.com>
2022-05-03 15:24:02 -0400

    refactor: change the names of serializers in petclub

--- personalProject/petclub/serializers.py
+++ personalProject/petclub/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 
 
-class DerivateSerializer(serializers.Serializer):
+class PersonSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     rut = serializers.CharField(max_length=12)
 
 
-class IntegrateSerializer(serializers.Serializer):
+class PetSerializer(serializers.Serializer):
     species = serializers.CharField(max_length=50)
     name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
