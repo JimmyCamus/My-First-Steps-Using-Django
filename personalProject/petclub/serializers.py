2794d12d076c33c8fb9f921b340515d39115d3cc
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-05-02 23:27:24 -0400

    feat: seriaizers added

---
+++ personalProject/petclub/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+
+class DerivateSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    rut = serializers.CharField(max_length=12)
+
+
+class IntegrateSerializer(serializers.Serializer):
+    species = serializers.CharField(max_length=50)
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    color = serializers.CharField(max_length=50)
